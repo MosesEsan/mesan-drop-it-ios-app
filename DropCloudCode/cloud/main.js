a8041ad79ddb0f6d0f2bc177954f973b785e3329
Moses Esan <mosesesan@hotmail.com>
2015-04-13 14:03:34 +0100

    Updated Cloud Code

--- DropCloudCode/cloud/main.js
+++ DropCloudCode/cloud/main.js
@@ -2,7 +2,7 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+    response.success("Hello world!");
 });
 
 
@@ -16,9 +16,9 @@ Parse.Cloud.afterSave("Posts", function(request) {
     var deviceId = request.object.get("deviceId");
     var location = request.object.get("location");
     var type = request.object.get("type");
+    var postText = request.object.get('text');
 
-
-    console.log("Type is "+type);
+    console.log(type);
 
     //Get the User object if it exist
     query = new Parse.Query("_User");
@@ -62,6 +62,45 @@ Parse.Cloud.afterSave("Posts", function(request) {
                 {
                     userObject.increment("points", 2);
                     userObject.save();
+
+
+                    //Create a notification object and save it
+                    var Notification = Parse.Object.extend("Notifications");
+                    var notification = new Notification();
+                    var message = "Someone liked your post: " + postText
+
+                    notification.set("message", message);
+                    notification.set("type", "Like");
+
+                    notification.save(null, {
+                        success: function(notification) {
+                            // Execute any logic that should take place after the object is saved.
+                            //send push notification
+                            var authorDeviceId = userObject.get("deviceId");
+                            var pushQuery = new Parse.Query(Parse.Installation);
+                            pushQuery.equalTo('deviceId', authorDeviceId);
+
+                            Parse.Push.send({
+                                where: pushQuery, // Set our Installation query
+                                data: {
+                                    alert: message
+                                }
+                            }, {
+                                success: function() {
+                                    // Push was successful
+                                    console.log("Pushed")
+                                },
+                                error: function(error) {
+                                    throw "Got an error " + error.code + " : " + error.message;
+                                }
+                            });
+                        },
+                        error: function(notification, error) {
+                            // Execute any logic that should take place if the save fails.
+                            // error is a Parse.Error with an error code and message.
+                            alert('Failed to create new object, with error code: ' + error.message);
+                        }
+                    });
                 }
             }else if (type == "Dislike"){
                 if (length > 0)
@@ -83,7 +122,75 @@ Parse.Cloud.afterSave("Posts", function(request) {
     });
 });
 
-function addNewUser(user)
-{
+Parse.Cloud.afterSave("Comments", function(request) {
 
-}
+    // Our "Comment" class has a "text" key with the body of the comment itself
+    var commentText = request.object.get('text');
+    var postId = request.object.get('postId');
+
+    var commenterDeviceId = request.object.get('deviceId');
+
+
+    var Post = Parse.Object.extend("Posts");
+    var query = new Parse.Query(Post);
+    query.equalTo("objectId", postId);
+    query.find({
+        success: function(post) {
+            // The object was retrieved successfully.
+
+            if (post.length > 0)
+            {
+                postObject = post[0];
+
+                var deviceId = postObject.get("deviceId");
+
+                if (deviceId != commenterDeviceId){
+
+                    //Create a notification object and save it
+                    var Notification = Parse.Object.extend("Notifications");
+                    var notification = new Notification();
+                    var message = "Someone left a comment on your post: " + commentText;
+
+                    notification.set("message", message);
+                    notification.set("type", "Comment");
+
+                    notification.save(null, {
+                        success: function(notification) {
+                            // Execute any logic that should take place after the object is saved.
+                            //send push
+                            var pushQuery = new Parse.Query(Parse.Installation);
+                            pushQuery.equalTo('deviceId', deviceId);
+
+                            Parse.Push.send({
+                                where: pushQuery, // Set our Installation query
+                                data: {
+                                    alert: message
+                                }
+                            }, {
+                                success: function() {
+                                    // Push was successful
+                                    console.log("Pushed")
+                                },
+                                error: function(error) {
+                                    throw "Got an error " + error.code + " : " + error.message;
+                                }
+                            });
+                        },
+                        error: function(notification, error) {
+                            // Execute any logic that should take place if the save fails.
+                            // error is a Parse.Error with an error code and message.
+                            alert('Failed to create new object, with error code: ' + error.message);
+                        }
+                    });
+
+
+
+                }
+            }
+        },
+        error: function(object, error) {
+            // The object was not retrieved successfully.
+            // error is a Parse.Error with an error code and message.
+        }
+    });
+});
